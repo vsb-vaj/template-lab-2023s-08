326ccea510d8f8d626f6cd4df3fcaa15c30f117f
Viktor Nawrath <viktor.nawrath@profiq.com>
2023-04-19 13:32:01 +0200

    Working code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/characters">Characters</Link>
+        </li>
+        <li>
+          <Link to="/episodes">Episodes</Link>
+        </li>
+        <li>
+          <Link to="/locations">Locations</Link>
+        </li>
+      </ul>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import {
+  Characters,
+  loader as charactersLoader,
+} from "./components/Characters";
+import {
+  CharacterDetail,
+  loader as characterDetailLoader,
+} from "./components/CharacterDetail";
+import {
+  CharacterComments,
+  loader as characterCommentsLoader,
+  action as characterCommentsAction,
+} from "./components/CharacterComments";
+import { Episodes, loader as episodesLoader } from "./components/Episodes";
+import {
+  EpisodeDetail,
+  loader as episodeDetailLoader,
+} from "./components/EpisodeDetail";
+import { Locations, loader as locationsLoader } from "./components/Locations";
+import {
+  LocationDetail,
+  loader as locationDetailLoader,
+} from "./components/LocationDetail";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <h1>Home</h1> },
+      { path: "characters", loader: charactersLoader, element: <Characters /> },
+      {
+        path: "characters/:characterId",
+        loader: characterDetailLoader,
+        element: <CharacterDetail />,
+        children: [
+          {
+            index: true,
+            loader: characterCommentsLoader,
+            action: characterCommentsAction,
+            element: <CharacterComments />,
+          },
+        ],
+      },
+      {
+        path: "episodes",
+        loader: episodesLoader,
+        element: <Episodes />,
+        children: [
+          {
+            path: ":episodeId",
+            loader: episodeDetailLoader,
+            element: <EpisodeDetail />,
+          },
+        ],
+      },
+
+      {
+        path: "locations",
+        loader: locationsLoader,
+        element: <Locations />,
+        children: [
+          {
+            path: ":locationId",
+            loader: locationDetailLoader,
+            element: <LocationDetail />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
